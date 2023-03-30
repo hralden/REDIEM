4f1fb006054c2cf85d3947111a37040bf0b38851
hralden <90366568+hralden@users.noreply.github.com>
2023-03-29 17:53:01 -0700

    Add files via upload

--- combined_code.ino
+++ combined_code.ino
@@ -1,220 +1,165 @@
 #include <Arduino.h>
-#include <SPI.h>
+#include <avr/io.h>
 #include <Wire.h>
-
+#include <SPI.h>
 #include "Adafruit_BLE.h"
 #include "Adafruit_BluefruitLE_SPI.h"
 #include "Adafruit_BluefruitLE_UART.h"
-#define ADDR_PTR 0xB0
-#define START_FREQ_R1 0x82
-#define START_FREQ_R2 0x83
-#define START_FREQ_R3 0x84
-
-#define FREG_INCRE_R1 0x85
-#define FREG_INCRE_R2 0x86
-#define FREG_INCRE_R3 0x87
-
-#define NUM_INCRE_R1 0x88
-#define NUM_INCRE_R2 0x89
-
-#define NUM_SCYCLES_R1 0x8A 
-#define NUM_SCYCLES_R2 0x8B 
-
-#define RE_DATA_R1 0x94
-#define RE_DATA_R2 0x95
-
-#define IMG_DATA_R1 0x96
-#define IMG_DATA_R2 0x97
-
-
-#define STATUS_REG 0x8F
 
 #include "BluefruitConfig.h"
+#define FACTORYRESET_ENABLE         1
+#define MINIMUM_FIRMWARE_VERSION    "0.6.6"
+#define MODE_LED_BEHAVIOUR          "MODE"
 
-#if SOFTWARE_SERIAL_AVAILABLE
-#include <SoftwareSerial.h>
-#endif
-//addresses & pin assignments
-const int MPU = 0x68; // MPU6050 I2C address
-const int BIOZ_ADDR = 0x0D;
 
-const int CTRL_REG = 0x80;
-const int CTRL_REG2 = 0x81;
-int flex_pin1 = 18; //flex1
-int flex_pin2 = 19; //flex2
-int flex_pin3 =20; //flex3
-int flex_pin4 = 21; //flex4
-int flex_pin5 =10; //flex3
-int flex_pin6 = 9; //flex4
-int emg_Pin = 6; //EMG
+//Flex Sensor Arduino Pins
+int flex_1 = 18;
+int flex_2 = 19;
+int flex_3 = 20;
+int flex_4 = 21;
+int flex_5 = 10;
+int flex_6 = 9;
 
-//variable def
-float AccX, AccY, AccZ;
-int flex1 = 0;
-int flex2 = 0;
-int flex3 = 0;
-int flex4 = 0;
-int bio_z = 0;
-int flex5 = 0;
-int flex6 = 0;
-byte flex1_byte = 0;
-byte flex2_byte = 0;
-byte flex3_byte = 0;
-byte flex4_byte = 0;
-byte flex5_byte = 0;
-byte flex6_byte = 0;
-float rawEMG = 0;
-byte rawEMG_byte = 0;
-int Vin = 5;
-int R0 = 15000;
+//EMG arduino pin
+int EMG_pin = 6;
+
+//Flex Resistor Values for Arduino Printing
+//remove when only bluetooth
 int R1 = 0;
 int R2 = 0;
 int R3 = 0;
 int R4 = 0;
 int R5 = 0;
 int R6 = 0;
-float Vout_flex1 = 0;
-float Vout_flex2 = 0;
-float Vout_flex3 = 0;
-float Vout_flex4 = 0;
-float Vout_flex5 = 0;
-float Vout_flex6 = 0;
+
+//Flex Variables
+int flex = 0;
+byte flex_byte =0;
+int vout = 0;
+int resistance;
+
+//EMG Variables
+float rawEMG = 0;
 float Vout_EMG = 0;
-short re;
-short img;
-double freq;
-double mag;
+
+
+//accelerometer variables
+float X, Y, Z;
+
+//Constants
+int Vin = 5;
+int R0 = 15000;
+const int MPU = 0x68; //accelerometer I2C address
+
+//BIOZ Registers
+const int BIOZ = 0x0D;
+const int BIOZ_PTR = 0xB0;
+const int START_FREQ_R1 = 0X82;
+const int START_FREQ_R2 = 0X83;
+const int START_FREQ_R3 = 0X84;
+const int FREQ_INCR_R1 = 0x85;
+const int FREQ_INCR_R2 = 0x86;
+const int FREQ_INCR_R3 = 0x87;
+const int NUM_INCRE_R1 = 0x88;
+const int NUM_INCRE_R2 = 0x89;
+const int NUM_CYCLES_R1 = 0x8A;
+const int NUM_CYCLES_R2 = 0x8B;
+const int REAL_DATA_R1 = 0x94;
+const int REAL_DATA_R2 = 0x95;
+const int IMAGINARY_DATA_R1 = 0x96;
+const int IMAGINARY_DATA_R2 = 0x97;
+const int CTRL_REG = 0x80;
+const int CTRL_REG2 = 0x81;
+const int STATUS_REG = 0x8F;
+
+//BIOZ Constants
+const float MCLK = 16.76*pow(10,6);
+const float start_freq = 50*pow(10,3);
+
+//BIOZ Variables
+short real;
+short imaginary;
+double frequency;
 double phase;
+double magnitude;
 double gain;
-double Impedance;
-double GF;
-double FFW;
-double wt;
-double BF;
-double tot = 0;
-double magcount = 0;
-double impcount = 0;
-double avgmag;
-double totimp = 0;
-double avgimp;
-  int i=0;
-    #define FACTORYRESET_ENABLE         1
-    #define MINIMUM_FIRMWARE_VERSION    "0.6.6"
-    #define MODE_LED_BEHAVIOUR          "MODE"
+double impedance;
+double gain_factor;
+double bioz;
 
-Adafruit_BluefruitLE_SPI ble(BLUEFRUIT_SPI_CS, BLUEFRUIT_SPI_IRQ, BLUEFRUIT_SPI_RST);
+Adafruit_BluefruitLE_SPI blue(BLUEFRUIT_SPI_CS, BLUEFRUIT_SPI_IRQ, BLUEFRUIT_SPI_RST);
 
-void setup(void)
-{
-  while (!Serial);  // required for Flora & Micro
-  delay(500);
 
+
+void setup() {
   Serial.begin(115200);
-
-
-  //Disable command echo
-  ble.echo(false);
-
-
-  ble.verbose(false);  
-
-  /* Wait for connection */
+  blue.factoryReset();
+  blue.echo(false);
+  blue.verbose(false);
+  blue.setMode(BLUEFRUIT_MODE_DATA);
   while (! ble.isConnected()) {
       delay(500);
   }
+  Wire.begin();
+  writeData(MPU, 0x6B, 0x00); //reset accelerometer
 
-
-  // Set module to DATA mode
-  ble.setMode(BLUEFRUIT_MODE_DATA);
+  writeData(BIOZ, CTRL_REG, 0x00);
+  writeData(BIOZ, CTRL_REG2, 0x10);
+  program_bioz_registers();
   
-  //ACCELEROMETER
-  writeData(0x6B, 0x00, MPU);
 
-  //BIO-Z
-  writeData(CTRL_REG, 0x0, BIOZ_ADDR);
-  writeData(CTRL_REG2, 0x10, BIOZ_ADDR);
+}
 
-  setup_BIOZ();
+void loop() {
+
+  //EMG
+  rawEMG = analogRead(EMG_pin);
+  Vout_EMG = (rawEMG/ 1024) * 5;
+  blue.print(Vout_EMG * 1000);
+
+
+  //Flex Sensors
+  R1 = calculate_flex_R(18);
+  blue.print(R1);
+  R2 = calculate_flex_R(19);
+  blue.print(R2);
+  R3 = calculate_flex_R(20);
+  blue.print(R3);
+  R4 = calculate_flex_R(21);
+  blue.print(R4);
+  R5 = calculate_flex_R(10);
+  blue.print(R5);
+  R6 = calculate_flex_R(9);
+  blue.print(R6);
+
+  //BIOZ
+  bioz = getBioz();
+  blue.print(bioz);
+  
+  //Accelerometer
+  Wire.beginTransmission(MPU);
+  Wire.write(0x3B);
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU, 6, true);
+  X = (Wire.read() << 8 | Wire.read()) / 16384.0; //read X axis data
+  blue.print(X);
+  Y = (Wire.read() << 8 | Wire.read()) / 16384.0; //read Y axis data
+  blue.print(Y);
+  Z = (Wire.read() << 8 | Wire.read()) /16384.0; // read Z axis data
+  blue.print(Z);
+
   
 }
 
-void loop(void)
-{
-
-//EMG
-    rawEMG = analogRead(emg_Pin);
-    rawEMG_byte = rawEMG / 4;
-    Vout_EMG = (rawEMG /1024)*5;
-    ble.print(Vout_EMG*1000);
-
-//FLEX SENSORS
-  flex1 = analogRead(flex_pin1);
-  flex2 = analogRead(flex_pin2);
-  flex3 = analogRead(flex_pin3);
-  flex4 = analogRead(flex_pin4);
-  flex5 = analogRead(flex_pin5);
-  flex6 = analogRead(flex_pin6);
-  flex1_byte = flex1 / 4;
-  flex2_byte = flex2 / 4;
-  flex3_byte = flex3 / 4;
-  flex4_byte = flex4 / 4;
-  flex5_byte = flex5 / 4;
-  flex6_byte = flex6 / 4;
-  Vout_flex1 = (flex1 * Vin)/1024.0;
-  Vout_flex2 = (flex2 * Vin)/1024.0;
-  Vout_flex3 = (flex3 * Vin)/1024.0;
-  Vout_flex4 = (flex4 * Vin)/1024.0;
-  Vout_flex5 = (flex5 * Vin)/1024.0;
-  Vout_flex6 = (flex6 * Vin)/1024.0;
-  R1 = ((Vin/Vout_flex1) - 1) * R0;
-  R2 = ((Vin/Vout_flex2) - 1) * R0;
-  R3 = ((Vin/Vout_flex3) - 1) * R0;
-  R4 = ((Vin/Vout_flex4) - 1) * R0;
-  R5 = ((Vin/Vout_flex5) - 1) * R0;
-  R6 = ((Vin/Vout_flex6) - 1) * R0;
-  ble.print(R1);
-  ble.print(R2);
-  ble.print(R3);
-  ble.print(R4);
-  ble.print(R5);
-  ble.print(R6);
-
-//BIO-Z
-bio_z = getImpedance();
-ble.print(bio_z);
-delay(4);
-
-    
-//ACCELEROMETER
-    Wire.beginTransmission(MPU);
-    Wire.write(0x3B); // Start with register 0x3B (ACCEL_XOUT_H)
-    Wire.endTransmission(false);
-    Wire.requestFrom(MPU, 6, true); // Read 6 registers total, each axis value is stored in 2 registers
-  //For a range of +-2g, we need to divide the raw values by 16384, according to the datasheet
-    AccX = (Wire.read() << 8 | Wire.read()) / 16384.0; // X-axis value
-    AccY = (Wire.read() << 8 | Wire.read()) / 16384.0; // Y-axis value
-    AccZ = (Wire.read() << 8 | Wire.read()) / 16384.0; // Z-axis value
-    // Send Accelerometer data to host via Bluefruit
-    ble.print(AccX);
-    ble.print(AccY);
-    ble.print(AccZ);
-
-// Echo received data
-  while ( ble.available() )
-  {
-    int c = ble.read();
-
-    Serial.print((char)c);
-
-    // Hex output too, helps w/debugging!
-    Serial.print(" [0x");
-    if (c <= 0xF) Serial.print(F("0"));
-    Serial.print(c, HEX);
-    Serial.print("] ");
-  }
+//function to calculate resistance 
+int calculate_flex_R(int pin){
+  flex = analogRead(pin);
+  vout = (flex * Vin) /1024;
+  resistance = ((Vin / vout) -1) * R0;
+  return resistance;
 }
 
-void writeData(int register_address, int data, int device_address){
+void writeData(int device_address, int register_address, int data){
   Wire.beginTransmission(device_address);
   Wire.write(register_address);
   Wire.write(data);
@@ -222,88 +167,61 @@ void writeData(int register_address, int data, int device_address){
   delay(1);
 }
 
-void readData(int device_address, int register_address){
+int readData(int device_address, int register_address){
   int data;
   Wire.beginTransmission(device_address);
-  Wire.write(ADDR_PTR);
+  Wire.write(BIOZ_PTR);
   Wire.write(register_address);
   Wire.endTransmission(true);
   delay(1);
-  Wire.requestFrom(device_address,1);
-
-  
-  Wire.requestFrom(BIOZ_ADDR,1);
-
-  if (Wire.available() >= 1){
-    data = Wire.read();
-  }
-  else {
-    data = -1;
-  }
-
-  delay(1);
+  Wire.requestFrom(device_address, 1);
+  data = Wire.read();
   return data;
 }
 
-void setup_BIOZ(){
-  writeData(CTRL_REG, 0x01, BIOZ_ADDR);
+void program_bioz_registers(){
+  //range 1 gain 1
+  writeData(BIOZ, CTRL_REG, 0x01);
+  //number of settling cycles 250 = 5ms
+  writeData(BIOZ, NUM_CYCLES_R1, 0x02);
+  writeData(BIOZ, NUM_CYCLES_R2, 0x7D);
 
-  writeData(NUM_SCYCLES_R1, 0x07, BIOZ_ADDR);
-  writeData(NUM_SCYCLES_R2, 0xFF, BIOZ_ADDR);
+  //start frequency
+  writeData(BIOZ, START_FREQ_R1, 0x19);
+  writeData(BIOZ, START_FREQ_R2, 0x99);
+  writeData(BIOZ, START_FREQ_R3, 0x99);
+
+  //Increment by 0
+  writeData(BIOZ, FREQ_INCR_R1, 0x00);
+  writeData(BIOZ, FREQ_INCR_R2, 0x00);
+  writeData(BIOZ, FREQ_INCR_R3, 0x00);
+
+  //number of increments = 0
+  writeData(BIOZ, NUM_INCRE_R1, 0x00);
+  writeData(BIOZ, NUM_INCRE_R2, 0x00);
   
 }
-  
-int getImpedance(){
-  setup_BIOZ();
-    // 1. Standby '10110000' Mask D8-10 of avoid tampering with gains
-  writeData(CTRL_REG,(readData(BIOZ_ADDR, CTRL_REG) & 0x07) | 0xB0, BIOZ_ADDR);
 
-  // 2. Initialize sweep
-  writeData(CTRL_REG,(readData(BIOZ_ADDR, CTRL_REG) & 0x07) | 0x10, BIOZ_ADDR);
-
-  // 3. Start sweep
-  writeData(CTRL_REG,(readData(BIOZ_ADDR, CTRL_REG) & 0x07) | 0x20, BIOZ_ADDR); 
-
-  while((readData(BIOZ_ADDR, STATUS_REG)& 0x07) < 4 ) {  // Check that status reg != 4, sweep not complete
-    delay(8); // delay between measurements
-
-    int flag = readData(BIOZ_ADDR, STATUS_REG)& 2;
-
-    if (flag==2) {
-
-      byte R1 = readData(BIOZ_ADDR, RE_DATA_R1);
-    
-      byte R2 = readData(BIOZ_ADDR, RE_DATA_R2);
-      re = (R1 << 8) | R2;
-
-      R1  = readData(BIOZ_ADDR, IMG_DATA_R1);
-      R2  = readData(BIOZ_ADDR, IMG_DATA_R2);
-      img = (R1 << 8) | R2;
-
-      freq = start_freq + i*incre_freq;
-      mag = sqrt(pow(double(re),2)+pow(double(img),2));
-                        tot = tot+mag;
-                        magcount = magcount+1;
-                        GF = (1/.22)/19.06;
-                        Impedance = 1/(GF*mag);
-                        if (Impedance < 100){
-                          impcount = impcount+1;
-                          totimp = totimp+Impedance;
-                        }             
-    
-      //Increment frequency
-      if((readData(BIOZ_ADDR, STATUS_REG) & 0x07) < 4 ){
-        writeData(CTRL_REG,(readData(BIOZ_ADDR, CTRL_REG) & 0x07) | 0x30, BIOZ_ADDR);
+double getBioz(){
+  int i = 0;
+  program_bioz_registers();
+  writeData(BIOZ, CTRL_REG, (readData(BIOZ, CTRL_REG) & 0x07) | 0xB0);
+  writeData(BIOZ, CTRL_REG, (readData(BIOZ, CTRL_REG) & 0x07) | 0x10);
+  writeData(BIOZ, CTRL_REG, (readData(BIOZ, CTRL_REG) & 0x07) | 0x20);
+  while ((readData(BIOZ, STATUS_REG) & 0x07) < 4){
+    int flag = readData(BIOZ, STATUS_REG) & 2;
+    if (flag == 2){
+      real = (readData(BIOZ, REAL_DATA_R1) << 8) | readData(BIOZ, REAL_DATA_R2);
+      imaginary = (readData(BIOZ, IMAGINARY_DATA_R1) << 8) | readData(BIOZ, IMAGINARY_DATA_R2);
+      magnitude = sqrt(pow(real, 2) + pow(imaginary, 2));
+      gain_factor = (1/.22)/19.6;
+      impedance = 1/ (gain_factor * magnitude);
+      if ((readData(BIOZ, STATUS_REG) & 0x07) < 4){
+        writeData(BIOZ, CTRL_REG, (readData(BIOZ, CTRL_REG) & 0x07) | 0x30);
         i++;
       }
-                        avgmag = tot/magcount;
-                        avgimp = totimp/impcount;
-                        wt = 118/2.205;
-                        FFW = (0.396*(pow(1.54,2)/(avgimp*1000))+0.143*wt+8.399)*1.37*2;
-                        BF = ((wt-FFW)/wt)*100;
-                        
     }
-                        
   }
-  return avgimp;
+  writeData(BIOZ, CTRL_REG, (readData(BIOZ, CTRL_REG) & 0x07) |0xA0);
+  return impedance;
 }
