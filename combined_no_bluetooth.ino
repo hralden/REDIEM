f819627bc3b207937a8b2f5c9a30447ead160e99
hralden <90366568+hralden@users.noreply.github.com>
2023-04-15 11:27:27 -0700

    Add files via upload

--- combined_no_bluetooth.ino
+++ combined_no_bluetooth.ino
@@ -7,8 +7,7 @@ int flex_1 = 18;
 int flex_2 = 19;
 int flex_3 = 20;
 int flex_4 = 21;
-int flex_5 = 10;
-int flex_6 = 9;
+
 
 //EMG arduino pin
 int EMG_pin = 6;
@@ -19,14 +18,13 @@ int R1 = 0;
 int R2 = 0;
 int R3 = 0;
 int R4 = 0;
-int R5 = 0;
-int R6 = 0;
+
 
 //Flex Variables
 int flex = 0;
 byte flex_byte =0;
-int vout = 0;
-int resistance;
+float vout = 0;
+float resistance;
 
 //EMG Variables
 float rawEMG = 0;
@@ -83,47 +81,38 @@ double bioz;
 void setup() {
   Serial.begin(115200);
   Wire.begin();
+
   writeData(MPU, 0x6B, 0x00); //reset accelerometer
 
   writeData(BIOZ, CTRL_REG, 0x00);
   writeData(BIOZ, CTRL_REG2, 0x10);
   program_bioz_registers();
+
+
   
 
 }
 
 void loop() {
 
-  //EMG
-  rawEMG = analogRead(EMG_pin);
-  Vout_EMG = (rawEMG/ 1024) * 5;
-  Serial.print(Vout_EMG * 1000);
-  Serial.print(",");
+
 
   //Flex Sensors
-  R1 = calculate_flex_R(18);
+  R1 = calculate_flex_R(flex_1);
   Serial.print(R1);
-  Serial.print(",");
-  R2 = calculate_flex_R(19);
+  Serial.print(", ");
+  R2 = calculate_flex_R(flex_2);
   Serial.print(R2);
-  Serial.print(",");
-  R3 = calculate_flex_R(20);
+  Serial.print(", ");
+  R3 = calculate_flex_R(flex_3);
   Serial.print(R3);
-  Serial.print(",");
-  R4 = calculate_flex_R(21);
+  Serial.print(", ");
+  R4 = calculate_flex_R(flex_4);
   Serial.print(R4);
   Serial.print(",");
-  R5 = calculate_flex_R(10);
-  Serial.print(R5);
-  Serial.print(",");
-  R6 = calculate_flex_R(9);
-  Serial.print(R6);
-  Serial.print(",");
-
-  //BIOZ
-  bioz = getBioz();
-  Serial.print(bioz);
-  Serial.print(",");
+  
+ 
+  
 
   //Accelerometer
   Wire.beginTransmission(MPU);
@@ -132,21 +121,35 @@ void loop() {
   Wire.requestFrom(MPU, 6, true);
   X = (Wire.read() << 8 | Wire.read()) / 16384.0; //read X axis data
   Serial.print(X);
-  Serial.print(",");
+  Serial.print(", ");
   Y = (Wire.read() << 8 | Wire.read()) / 16384.0; //read Y axis data
-  Serial.print(Y);
-  Serial.print(",");
+ Serial.print(Y);
+  Serial.print(", ");
   Z = (Wire.read() << 8 | Wire.read()) /16384.0; // read Z axis data
-  Serial.println(Z);
+  Serial.print(Z);
+  Serial.print(", ");
 
+  //EMG
+  rawEMG = analogRead(EMG_pin);
+  Vout_EMG = (rawEMG/ 1024) * 5;
+  Vout_EMG = Vout_EMG * 1000;
+  Serial.print(Vout_EMG);
+  Serial.print(", ");
+
+ //BIOZ
+  bioz = getBioz();
+  Serial.println(bioz);
+
+//delay
+  delay(8);
   
 }
 
 //function to calculate resistance 
 int calculate_flex_R(int pin){
   flex = analogRead(pin);
-  vout = (flex * Vin) /1024;
-  resistance = ((Vin / vout) -1) * R0;
+  vout = float((flex * Vin) /1024.0);
+  resistance = float(((Vin / vout) -1)) * R0;
   return resistance;
 }
 
